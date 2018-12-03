866c2cfd20e88cfc4041e7352294829e01cc825f
ollydevulder <ollydevulder@gmail.com>
2018-12-03 19:34:47 +0000

    test

--- 18/3/part1.py
+++ 18/3/part1.py
@@ -5,7 +5,9 @@ with open("input.txt") as input:
     for claim in raw_claims:
         claims[claim[0]] = [[int(c) for c in claim[2].strip(':').split(',')], [int(c) for c in claim[3].split('x')]]
 square_inches = []
+visited = []
 count = 0
+raw_claims = None
 print("Overlaps found:\n")
 for claim in claims:
     this_claim = claims[claim]
@@ -13,8 +15,9 @@ for claim in claims:
         y+=this_claim[0][1]
         for x in range(this_claim[1][0]):
             x+=this_claim[0][0]
-            if [x, y] in square_inches:
+            if [x, y] in square_inches and [x, y] not in visited:
                 count+=1
+                visited.append([x, y])
                 stdout.write("\r"+str(count))
                 stdout.flush()
             square_inches.append([x,y])
